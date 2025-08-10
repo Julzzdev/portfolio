d27e1466a561d7d9eeee2eecfccd58d51b34afb8
Julio Olvera <julioolveraunt@gmail.com>
2025-08-10 13:40:50 -0600

    feat: interest collection and stuff

---
+++ src/collections/anime/Gundam.md
@@ -0,0 +1,8 @@
+---
+title: "Mobile Suit Gundam 0079"
+description: ""
+releaseDate:
+genres: ["science fiction"]
+rating: 8
+image: "https://static.wikia.nocookie.net/doblaje-fanon/images/b/bd/DVD-0079-FullTV.jpg/revision/latest?cb=20190714221724&path-prefix=es"
+---
--- src/content.config.ts
+++ src/content.config.ts
@@ -5,11 +5,19 @@ import { defineCollection, z } from 'astro:content';
 import { glob, file } from 'astro/loaders';
 
 // 3. Define your collection(s)
-const anime = defineCollection({ /* ... */ });
+const anime = defineCollection({ loader: glob({ pattern: './collections/anime/*.md' }), schema: z.object({
+  title: z.string(),
+  description: z.string().optional(),
+  releaseDate: z.string().optional(),
+  genres: z.array(z.string()).optional(),
+  rating: z.number().min(0).max(10).optional(),
+  image: z.string().optional(),
+}) });
 const books = defineCollection({ /* ... */ });
-const games = defineCollection({ /* ... */ });
-const lorcana = defineCollection({ /* ... */ });
+const cardGames = defineCollection({ /* ... */ });
 const manga = defineCollection({ /* ... */ });
+const projects = defineCollection({ /* ... */ });
+const videoGames = defineCollection({ /* ... */ });
 
 // 4. Export a single `collections` object to register your collection(s)
-export const collections = { anime, books, games, lorcana, manga };
+export const collections = { anime, books, cardGames, manga, projects, videoGames };
